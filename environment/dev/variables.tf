d0f5bf6ba21e05b6871c3f38fb3a7080e6bdfb60
ATOJ5 <dgc105@gmail.com>
2025-03-08 21:41:18 +0100

    update

--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -2,16 +2,19 @@ variable "bucket_count" {
     description = "Número de buckets a crear"
     type        = number
     default     = 2
+    sensitive   = false
 }
 
 variable "region" {
     description = "Región de AWS"
     type        = string
     default     = "us-east-1"
+    sensitive   = false
 }
 
 variable "bucket_prefix" {
     description = "Prefijo para los nombres de los buckets"
     type        = string
     default     = "proveedores-regional-bucket-prueba-atoj"
+    sensitive   = false
 }
