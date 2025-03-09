a3d46e4722fa2582ae6cc5c7f4ce33ffa4df4df4
ATOJ5 <dgc105@gmail.com>
2025-03-09 20:10:24 +0100

    vpc update

--- modules/networking/vpc.tf
+++ modules/networking/vpc.tf
@@ -15,12 +15,24 @@ resource "aws_subnet" "public_subnet" {
     cidr_block = var.public_subnet_virginia
     map_public_ip_on_launch = true # Default is false so you need to turn into true if u want to make it publick really.
     provider = aws.virginia
+    tags = {
+        Name = "Public Subnet Virginia"
+        env = aws_vpc.vpc_virginia.tags["env"] # You can capture env from resources using this key. 
+        description = "public subnet description example"
+        CIDR = var.public_subnet_virginia
+    }
 }
 
 resource "aws_subnet" "private_subnet" {
     vpc_id = aws_vpc.vpc_virginia.id
     cidr_block = var.private_subnet_virginia
     provider = aws.virginia
+    tags = {
+        Name = "Private Subnet Virginia"
+        env = aws_vpc.vpc_virginia.tags["env"]
+        description = "Private Subnet Description example"
+        CIDR = var.private_subnet_virginia
+    }
 }
 # Calling object variable
 resource "aws_vpc" "vpc_ohio" {
