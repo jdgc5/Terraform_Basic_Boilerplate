7a99e933f38521d3e43c0c7b7ff739b58538bc08
ATOJ5 <dgc105@gmail.com>
2025-03-10 22:43:10 +0100

    adding data to ec2 instance.

--- modules/compute/ec2/ec2.tf
+++ modules/compute/ec2/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "public_instance" {
     ami                     = "ami-08b5b3a93ed654d19"
     instance_type           = "t2.micro" # free tier.
     subnet_id =  var.subnet_id
+    key_name = data.aws_key_pair.key_ec2.key_name
 
     tags = {
         Name = "EC2_Virginia_Test"
